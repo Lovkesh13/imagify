9c4eb087366522b0178eefe17f8f01853e8adddc
Lovkesh <lovkeshbarowalia@gmail.com>
2025-01-15 23:26:05 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,24 +1,29 @@
-import express from 'express'
-import cors from 'cors'
-import 'dotenv/config'
-import connectDB from './config/mongodb.js'
-import userRouter from './routes/userRoutes.js'
-import imageRouter from './routes/imageRoutes.js'
+import express from 'express';
+import cors from 'cors';
+import 'dotenv/config';
+import connectDB from './config/mongodb.js';
+import userRouter from './routes/userRoutes.js';
+import imageRouter from './routes/imageRoutes.js';
 
-const PORT = process.env.PORT || 4000
-const app = express()
+const PORT = process.env.PORT || 4000;
+const app = express();
 
-app.use(express.json())
-app.use(cors())
-await connectDB()
+const allowedOrigins = ['https://imagify-frontend-itws.onrender.com']; 
+app.use(cors({
+    origin: allowedOrigins,
+    credentials: true,
+}));
+
+app.use(express.json());
+await connectDB();
 
 app.use('/api/user', userRouter);
 app.use('/api/image', imageRouter);
 
-app.get('/', (req, res) =>{
-    res.send("API working")
-})
+app.get('/', (req, res) => {
+    res.send("API working");
+});
 
-app.listen(PORT, ()=>{
-    console.log("server running on port " + PORT);
-})
+app.listen(PORT, () => {
+    console.log("Server running on port " + PORT);
+});
